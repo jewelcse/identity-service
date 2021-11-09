79996d097d388deb5244e0586dbf74d9feef8a7c
jewelcsebu <jewelchowdhurybucse@gmail.com>
2021-11-09 16:05:11 +0600

    user, authority and role model added

---
+++ src/main/java/com/identityservice/model/User.java
@@ -0,0 +1,39 @@
+package com.identityservice.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    @Column(name = "isActive")
+    private boolean isActive;
+
+    @ManyToMany(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    private List<Authority> authorities;
+}
